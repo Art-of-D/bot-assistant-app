e5f1f05f7f1be19a32139b2fc60398b0027e1017
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2025-01-07 15:42:56 -0700

    Class Birthday has been created

--- bot/internal/birthday.py
+++ bot/internal/birthday.py
@@ -1,5 +1,21 @@
+from datetime import datetime
+
 from bot.internal.field import Field
 from bot.decorators.errorhandler import input_error
 
+@input_error
 class Birthday(Field):
-  pass
+    _dateFormat = "%d.%m.%Y"
+
+    def __init__(self, value):
+        try:
+            self.value = datetime.strptime(value, self._dateFormat)
+        except ValueError:
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
+
+    def __str__(self):
+        return self.value.strftime(self._dateFormat)
+
+    @staticmethod
+    def format():
+        return Birthday._dateFormat
