0cac5e92bacebbc23893f60825a4fafac81e0729
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2025-01-08 15:23:35 -0700

    Class birthday updated, get_upcoming_birthday method created

--- bot/internal/birthday.py
+++ bot/internal/birthday.py
@@ -9,9 +9,18 @@ class Birthday(Field):
 
     def __init__(self, value):
         try:
-            self.value = datetime.strptime(value, self._dateFormat)
+            # Парсимо дату
+            parsed_date = datetime.strptime(value, self._dateFormat)
+            
+            # Валідуємо вік
+            self._validate_age(parsed_date)
+            
+            # Якщо все добре, зберігаємо дату
+            self.value = parsed_date
         except ValueError:
             raise ValueError("Invalid date format. Use DD.MM.YYYY")
+        except AssertionError as e:
+            raise ValueError(str(e))
 
     def __str__(self):
         return self.value.strftime(self._dateFormat)
@@ -19,3 +28,18 @@ class Birthday(Field):
     @staticmethod
     def format():
         return Birthday._dateFormat
+
+    def get_birthday(self):
+        return self.value
+
+    @staticmethod
+    def _validate_age(birth_date):
+        """Перевіряє, чи вік у межах від 0 до 150 років."""
+        today = datetime.today()
+        age = (today - birth_date).days // 365
+
+        # Перевіряємо, чи вік у розумних межах
+        if age < 0:
+            raise AssertionError("The date of birth cannot be in the future.")
+        if age > 125:
+            raise AssertionError("The age cannot exceed 125 years.")
